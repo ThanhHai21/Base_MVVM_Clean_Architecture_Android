af549aa1541b3f61ddd0367602ffdb9aaeb32038
ThanhHai <haidance052@gmail.com>
2023-07-27 16:37:16 +0700

    Create base MVVM with rest api

---
+++ app/src/main/java/com/fatherofapps/androidbase/di/Scope.kt
@@ -0,0 +1,12 @@
+package com.fatherofapps.androidbase.di
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class JsonPlaceHolderSite(
+)
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class StackOverFlowSite()
